1630b66f5fed0ed6a484eab182c238dae45ed287
MaksimTimofeev <Maksim.Timofeev@homecredit.ru>
2024-07-24 13:27:10 +0400

    add ErrorBoundary

---
+++ app-2/src/ErrorBoundary.tsx
@@ -0,0 +1,31 @@
+import { Component, ErrorInfo, FC, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  error: null | Error;
+}
+
+export class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { error: null };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    this.setState({ error });
+  }
+
+  render(): ReactNode {
+    const { error } = this.state;
+    const { children } = this.props;
+
+    if (error) {
+      return <div>Произошла ошибка при подгрузке модуля (ErrorBoundary)</div>;
+    }
+
+    return children;
+  }
+}
