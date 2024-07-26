7fb686f8c288b4f8680479ef79bd522f95ad5491
MaksimTimofeev <Maksim.Timofeev@homecredit.ru>
2024-07-26 11:20:46 +0400

    fix configs

--- rsbuild/app-1 (HOST)/rsbuild.config.ts
+++ rsbuild/app-1 (HOST)/rsbuild.config.ts
@@ -3,6 +3,8 @@ import { pluginReact } from '@rsbuild/plugin-react';
 import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
 import { pluginStyledComponents } from '@rsbuild/plugin-styled-components';
 
+import { dependencies } from './package.json';
+
 // Для локальной разработки без Module Federation (Ускоряет работу)
 const withoutMF = JSON.stringify(process.env.WITHOUT_MF);
 
@@ -17,9 +19,13 @@ export default defineConfig(() => ({
   server: { port: 3001 },
   output: {
     copy: [{ from: './static' }],
+    cleanDistPath: true,
   },
   dev: {
     assetPrefix: 'http://localhost:3001',
+    hmr: true,
+    liveReload: false,
+    progressBar: true,
   },
   tools: {
     rspack: (config, { appendPlugins, addRules }) => {
@@ -32,9 +38,27 @@ export default defineConfig(() => ({
                 name: 'app1',
                 filename: 'app1.js',
                 remotes: {
-                  app2: 'app2@http://localhost:3002/mf-manifest.json',
+                  app2: 'app2@http://localhost:3002/app2-manifest.json',
+                  app3: 'app3@http://localhost:3003/app3-manifest.json',
                 },
-                shared: ['react', 'react-dom', 'urql', 'graphql'],
+                shared: {
+                  react: {
+                    singleton: true,
+                    requiredVersion: dependencies.react,
+                  },
+                  'react-dom': {
+                    singleton: true,
+                    requiredVersion: dependencies['react-dom'],
+                  },
+                  urql: {
+                    singleton: true,
+                    requiredVersion: dependencies.urql,
+                  },
+                  graphql: {
+                    singleton: true,
+                    requiredVersion: dependencies.graphql,
+                  },
+                },
                 runtimePlugins: [
                   require.resolve('./offlineRemotePlugin.ts'),
                   require.resolve('./shared-strategy.ts'),
--- rsbuild/app-3/rsbuild.config.ts
+++ rsbuild/app-3/rsbuild.config.ts
@@ -2,6 +2,8 @@ import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
+import { dependencies } from './package.json';
+
 // Для локальной разработки без Module Federation (Ускоряет работу)
 const withoutMF = JSON.stringify(process.env.WITHOUT_MF);
 
@@ -16,9 +18,13 @@ export default defineConfig(() => ({
   server: { port: 3003 },
   output: {
     copy: [{ from: './static' }],
+    cleanDistPath: true,
   },
   dev: {
     assetPrefix: 'http://localhost:3003',
+    hmr: true,
+    liveReload: false,
+    progressBar: true,
   },
   tools: {
     rspack: (config, { appendPlugins, addRules }) => {
@@ -34,11 +40,31 @@ export default defineConfig(() => ({
                   './Component3': './src/Component3/Component3.tsx',
                   './handlers': './src/mocks/handlers.ts',
                 },
-                shared: ['react', 'react-dom', 'urql', 'graphql'],
+                shared: {
+                  react: {
+                    singleton: true,
+                    requiredVersion: dependencies.react,
+                  },
+                  'react-dom': {
+                    singleton: true,
+                    requiredVersion: dependencies['react-dom'],
+                  },
+                  urql: {
+                    singleton: true,
+                    requiredVersion: dependencies.urql,
+                  },
+                  graphql: {
+                    singleton: true,
+                    requiredVersion: dependencies.graphql,
+                  },
+                },
                 runtimePlugins: [
                   require.resolve('./offlineRemotePlugin.ts'),
                   require.resolve('./shared-strategy.ts'),
                 ],
+                manifest: {
+                  fileName: 'app3-manifest.json',
+                },
               }),
             ],
       );
